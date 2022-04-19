a07d84b94a2c137ee9d2197ec6a4010c825b5ba3
林子傑 <jeffreyjeffreylin98@gmail.com>
2022-04-19 18:31:17 -0400

    feat: connect to mongodb

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
-const http = require('http');
+const http = require('http')
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+
+// MongoDB
+const { MongoClient } = require("mongodb");
+const client = new MongoClient("mongodb+srv://test:test@cluster0.qynhu.mongodb.net/Cluster0?retryWrites=true&w=majority");
+client.connect();
+collection = client.db("engagementMapDB").collection("feedback");
 
 // For frontend hot reloading
 const livereload = require("livereload");
@@ -16,11 +22,11 @@ app.use(connectLivereload());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-  });
+  res.sendFile(__dirname + '/public/index.html');
+});
 
 app.get('/student', (req, res) => {
-    res.sendFile(__dirname + '/public/student.html');
+  res.sendFile(__dirname + '/public/student.html');
 });
 
 app.get('/dashboard', (req, res) => {
@@ -28,27 +34,28 @@ app.get('/dashboard', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    socket.broadcast.emit('hi');
+  socket.broadcast.emit('hi');
 
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 
-    socket.on('feedback', (msg) => {
-        try {
-            msgJson = JSON.parse(msg);
-            console.log(msgJson);
-            userId = msgJson.id;
-            io.emit('dashboard-update', 'new feedback from '+userId);
-        }
-        catch {
-            console.log('Invalid JSON');
-            return;
-        }
-      });
+  socket.on('feedback', async (msg) => {
+    try {
+      msgJson = JSON.parse(msg);
+      console.log(msgJson);
+      userId = msgJson.id;
+      await collection.insertOne(msgJson);
+      io.emit('dashboard-update', 'new feedback from ' + userId);
+    }
+    catch {
+      console.log('Invalid JSON');
+      return;
+    }
+  });
 });
 
 
 server.listen(3000, () => {
-    console.log('listening on *:3000');
-  });
+  console.log('listening on *:3000');
+});
